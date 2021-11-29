6f16b0e5bbb50686e6491a6c4b4f85f25fb87d21
redundant4u <rafch@naver.com>
2021-11-29 10:22:23 +0900

    Add 'list' resource

--- server/nestjs/src/app.module.ts
+++ server/nestjs/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ListModule } from './list/list.module';
+
 @Module({
-  imports: [],
+  imports: [ListModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server/nestjs/src/list/list.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { ListService } from './list.service';
+
+@Controller('list')
+export class ListController {
+  constructor(private readonly listService: ListService) {}
+
+  @Get()
+  @Render('list')
+  root() {
+    return { msg: 'nestjs'}
+  }
+}
