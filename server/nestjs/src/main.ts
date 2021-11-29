54a89bdd89c84ded8a494111db804e4364471933
redundant4u <rafch@naver.com>
2021-11-29 10:23:23 +0900

    Add hbs template

--- server/nestjs/src/main.ts
+++ server/nestjs/src/main.ts
@@ -1,13 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
-import compression from 'compression';
+import * as compression from 'compression';
+import * as hbs from 'hbs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors();
   app.use(compression());
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  
+  hbs.registerPartials(join(__dirname, '..', 'views/partials'));
+  app.setViewEngine('hbs');
 
   await app.listen(3000);
 }
