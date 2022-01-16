dfcb79b8e2ac2844363a29992fcd362336fa086f
redundant4u <rafch@naver.com>
2022-01-16 22:20:34 +0900

    Fix typeorm error & Add seeder

--- server/nest-next/src/app.module.ts
+++ server/nest-next/src/app.module.ts
@@ -1,17 +1,32 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { RenderModule } from 'nest-next';
-import Next from 'next';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+import { RenderModule } from "nest-next";
+import Next from "next";
 
-import { AppController } from './app.controller';
-import { ListModule } from './list/list.module';
+import { AppController } from "./app.controller";
+import { ListModule } from "./list/list.module";
+
+import { ListEntity } from "@entity/list.entity";
+import { CorpEntity } from "@entity/corp.entity";
+import { IndustryEntity } from "@entity/industry.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT as string),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: false,
+      entities: [ListEntity, CorpEntity, IndustryEntity],
+    }),
     RenderModule.forRootAsync(
       Next({
-        dev: process.env.NODE_ENV !== 'production',
+        dev: process.env.NODE_ENV !== "production",
         conf: { useFilesystemPublicRoutes: false } as any,
       }),
     ),
